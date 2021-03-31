0f656b0b134262847a2c5b5ae6c148684b6966a6
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-03-31 17:49:08 +0900

    added the gaussian blur function

--- main.js
+++ main.js
@@ -24,15 +24,51 @@ var generateGaussianKernel = function(k){
             arr[i*n+j] = 1/(2*Math.PI*sigma2)*(Math.E**(((i-k)**2+(j-k)**2)/(-2*sigma2)));
         }
     }
+    //this code no longer necessary because offset is calculated on the user side
+    /*
     var sum = 0;
     for(var i = 0; i < arr.length; i++){
         sum += arr[i];
     }
     for(var i = 0; i < arr.length; i++){
         arr[i] /= sum;
-    }
+    }*/
     return arr;
 };
+
+var applyGaussianBlur = function(imgdata){
+    var data = imgdata.data;
+    var width = imgdata.width;
+    var height = imgdata.height;
+    var data1 = new Uint8ClampedArray(data.length);
+    var k = 2;
+    var n = k*2+1;//true width of the kernel
+    var kernel = generateGaussianKernel(k);
+    for(var y = 0; y < height; y++){
+        for(var x = 0; x < height; x++){
+            var kernelSum = 0;
+            var valueSum = 0;
+            
+            for(var i = 0; i < k*2+1; i++){
+                for(var j = 0; j < k*2+1; j++){
+                    var x1 = (y+i-k);
+                    var y1 = (x+j-k);
+                    if(x1 < 0 || y1 < 0 || x1 >= width || y1 >= height){
+                        continue;//kernel out of range
+                    }
+                    var idx1 = (y+i-k)*width+(x+j-k);
+                    var val = data[idx1];
+                    var kval = kernel[i*n+j];
+                    valueSum += kval*val;
+                }
+            }
+            var idx = y*width+x;
+            data1[idx] = valueSum/kernelSum;
+        }
+    }
+    imgdata.data = data1;
+};
+
 
 var getImageDataFromFile = function(file){
     return new Promise((resolve,reject)=>{
